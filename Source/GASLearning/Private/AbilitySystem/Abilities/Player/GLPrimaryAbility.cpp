737bd62a8c3995dc4ce105b8835f2d6a5144b66f
Timothé <timothe.lorin@gmail.com>
2026-01-06 16:39:15 +0100

    [ADD] Custom primary ability cpp class + hit react send event

---
+++ Source/GASLearning/Private/AbilitySystem/Abilities/Player/GLPrimaryAbility.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/Player/GLPrimaryAbility.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Engine/OverlapResult.h"
+#include "GameplayTags/GLTags.h"
+
+TArray<AActor*> UGLPrimaryAbility::HitBoxOverlapTest()
+{
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+	
+	// Ensure that the overlap test ignores the Avatar actor
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActors(ActorsToIgnore);
+	
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+	
+	TArray<FOverlapResult> OverlapResults;
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(HitBoxRadius);
+	
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector() * HitBoxForwardOffset;
+	const FVector HitBoxLocation = GetAvatarActorFromActorInfo()->GetActorLocation() + Forward + FVector(0.f, 0.f, HitBoxElevationOffset);
+	
+	GetWorld()->OverlapMultiByChannel(OverlapResults, HitBoxLocation, FQuat::Identity, ECC_Visibility, Sphere, QueryParams, ResponseParams);
+	
+	
+	TArray<AActor*> HitActors;
+	
+	for (const FOverlapResult& Result : OverlapResults)
+	{
+		if (!IsValid(Result.GetActor())) continue;
+		
+		HitActors.Add(Result.GetActor());
+	}
+	
+	if (bDrawDebugs)
+	{
+		DrawHitBoxOverlapDebugs(OverlapResults, HitBoxLocation);
+	}
+	
+	return HitActors;
+}
+
+void UGLPrimaryAbility::SendHitReactEventToActors(const TArray<AActor*>& HitActors)
+{
+	for (AActor* HitActor : HitActors)
+	{
+		FGameplayEventData Payload;
+		Payload.Instigator = GetAvatarActorFromActorInfo();
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(HitActor, GLTags::Events::Enemy::HitReact, Payload);
+	}
+}
+
+void UGLPrimaryAbility::DrawHitBoxOverlapDebugs(const TArray<FOverlapResult>& OverlapResults,
+                                                const FVector& HitBoxLocation) const
+{
+	DrawDebugSphere(GetWorld(), HitBoxLocation, HitBoxRadius, 16, FColor::Red, false, 3.f);
+	
+	for (const FOverlapResult& Result : OverlapResults)
+	{
+		if (IsValid(Result.GetActor()))
+		{
+			FVector DebugLocation = Result.GetActor()->GetActorLocation();
+			DebugLocation.Z += 100.f;
+			DrawDebugSphere(GetWorld(), DebugLocation, 30.f, 10, FColor::Green, false, 3.f);
+		}
+	}
+}
---
+++ Source/GASLearning/Public/AbilitySystem/Abilities/Player/GLPrimaryAbility.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GLGameplayAbility.h"
+#include "GLPrimaryAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASLEARNING_API UGLPrimaryAbility : public UGLGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	TArray<AActor*> HitBoxOverlapTest();
+	
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void SendHitReactEventToActors(const TArray<AActor*>& HitActors);
+	
+private:
+	void DrawHitBoxOverlapDebugs(const TArray<FOverlapResult>& OverlapResults, const FVector& HitBoxLocation) const;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	float HitBoxRadius = 100.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	float HitBoxForwardOffset = 200.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Abilities")
+	float HitBoxElevationOffset = 20.f;
+};
