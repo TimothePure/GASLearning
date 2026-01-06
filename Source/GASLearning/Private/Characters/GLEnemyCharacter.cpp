e9988c30b46b0183ea5929abf7eb409a9195b2d7
Timothé <timothe.lorin@gmail.com>
2026-01-06 12:07:16 +0100

    [ADD] Base enemies

---
+++ Source/GASLearning/Private/Characters/GLEnemyCharacter.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/GLEnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+AGLEnemyCharacter::AGLEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* AGLEnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGLEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!IsValid(GetAbilitySystemComponent())) return;
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+	if (HasAuthority())
+	{
+		GiveStartupAbilities();
+	}
+}
+
---
+++ Source/GASLearning/Public/Characters/GLEnemyCharacter.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GLBaseCharacter.h"
+#include "GLEnemyCharacter.generated.h"
+
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GASLEARNING_API AGLEnemyCharacter : public AGLBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGLEnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
