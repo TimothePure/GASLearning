1d70771d3e5d2b804f371ada478e26a9d63e134d
Timothé <timothe.lorin@gmail.com>
2026-01-06 11:16:10 +0100

    [ADD] Base ability class + play montage for primary

---
+++ Source/GASLearning/Public/AbilitySystem/Abilities/GLGameplayAbility.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GLGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASLEARNING_API UGLGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Debug")
+	bool bDrawDebugs = false;
+};
